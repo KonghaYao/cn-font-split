1b52ad3f1467aa02598bbdc55cddd92fd5ac4be9
konghayao <dongzhongzhidong@qq.com>
2021-11-04 18:12:58 +0800

    fixed：优化速度

--- src/CutTargetFont.js
+++ src/CutTargetFont.js
@@ -1,13 +1,14 @@
-import { ReadFontUnicode } from "./utils/FontUtils.js";
+// import { ReadFontUnicode } from "./utils/FontUtils.js";
 import { intersection, chunk } from "lodash-es";
 
 // 分包系统
 export default async function ({ file }, { other, TC, SC }, chunkOptions = {}) {
-    const allCode = await ReadFontUnicode(file);
+    // const allCode = await ReadFontUnicode(file);
     const total = { other, TC, SC };
     const last = Object.entries(total).reduce((col, [key, value]) => {
         if (value.length) {
-            const subset = intersection(value, allCode);
+            // const subset = intersection(value, allCode);
+            const subset = value;
             const size = Math.ceil(subset.length / chunkOptions[key]);
             const result = chunk(subset, size);
             col.set(key, result);
