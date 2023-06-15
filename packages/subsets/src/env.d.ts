b3d0909886ff88d3f70b2abee141cc570c4d1808
江夏尧 <3446798488@qq.com>
2023-06-15 14:12:41 +0800

    chore: 更换版本添加类型注释成功

--- packages/subsets/src/env.d.ts
+++ packages/subsets/src/env.d.ts
@@ -2,7 +2,3 @@ declare module "*.html" {
     const a: string;
     export default a;
 }
-declare module "@chinese-fonts/wawoff2" {
-    export const compress: any;
-    export const decompress: any;
-}
