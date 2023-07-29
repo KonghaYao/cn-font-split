093eeb662d029bf52f13dac5ead818e41eb15d3f
江夏尧 <3446798488@qq.com>
2023-07-29 16:37:10 +0800

    fixed: 修复browser打包错误问题

--- packages/subsets/test/deno.test.js
+++ packages/subsets/test/deno.test.js
@@ -13,8 +13,8 @@ Assets.redefine({
 console.log(Assets);
 fontSplit({
     destFold: './temp',
-    // FontPath: '../demo/public/SmileySans-Oblique.ttf',
-    FontPath: './test/temp/MaokenAssortedSans1.30.ttf',
+    FontPath: '../demo/public/SmileySans-Oblique.ttf',
+    // FontPath: './test/temp/MaokenAssortedSans1.30.ttf',
     targetType: 'woff2',
     // subsets: JSON.parse(await fs.readFile("./subsets/misans.json", "utf-8")),
     // previewImage: {},
