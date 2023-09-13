f8f7b7b8ade4426dd6d0c8e993f5cfc1365318e6
江夏尧 <3446798488@qq.com>
2023-09-13 21:56:08 +0800

    style: 格式化代码

--- packages/subsets/test/deno.online.test.js
+++ packages/subsets/test/deno.online.test.js
@@ -1,24 +1,24 @@
-import "https://deno.land/x/process@v0.3.0/mod.ts";
+import 'https://deno.land/x/process@v0.3.0/mod.ts';
 
 import {
     fontSplit,
     Assets,
     DenoAdapter,
-} from "https://cdn.jsdelivr.net/npm/@konghayao/cn-font-split@4.3.2/dist/browser/index.js";
+} from 'https://cdn.jsdelivr.net/npm/@konghayao/cn-font-split@4.6.0/dist/browser/index.js';
 
 await DenoAdapter();
-const root = "https://cdn.jsdelivr.net/npm/@konghayao/cn-font-split@4.3.2";
+const root = 'https://esm.sh/@konghayao/cn-font-split@4.6.0';
 Assets.redefine({
-    "hb-subset.wasm": root + "/dist/browser/hb-subset.wasm",
-    "cn_char_rank.dat": root + "/dist/browser/cn_char_rank.dat",
-    "unicodes_contours.dat": root + "/dist/browser/unicodes_contours.dat",
+    'hb-subset.wasm': root + '/dist/browser/hb-subset.wasm',
+    'cn_char_rank.dat': root + '/dist/browser/cn_char_rank.dat',
+    'unicodes_contours.dat': root + '/dist/browser/unicodes_contours.dat',
 });
 
 console.log(Assets);
 fontSplit({
-    destFold: "./temp",
-    FontPath: "../demo/public/SmileySans-Oblique.ttf",
-    targetType: "woff2",
+    destFold: './temp',
+    FontPath: '../demo/public/SmileySans-Oblique.ttf',
+    targetType: 'woff2',
     // subsets: JSON.parse(await fs.readFile("./subsets/misans.json", "utf-8")),
     // previewImage: {},
     threads: {},
