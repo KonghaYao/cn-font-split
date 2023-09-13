b7c4cec7c331bc5afeeb0a6e04e628cbd96db712
江夏尧 <3446798488@qq.com>
2023-09-13 21:56:08 +0800

    style: 格式化代码

--- packages/subsets/test/getAllUnicodes.mjs
+++ packages/subsets/test/getAllUnicodes.mjs
@@ -1,15 +1,14 @@
-import fs from "fs/promises";
-import { hbjs, autoSubset } from "../dist/index.js";
-import { Font } from "fonteditor-core";
+import fs from 'fs/promises';
+import { hbjs, autoSubset } from '../dist/index.js';
 // import Reader from "fonteditor-core/src/ttf/table/ttfreader.js";
 
 const wasmBuffer = await fs.readFile(
-    "./node_modules/@konghayao/harfbuzzjs/hb-subset.wasm"
+    './node_modules/@konghayao/harfbuzzjs/hb-subset.wasm'
 );
 const source = await WebAssembly.instantiate(wasmBuffer);
 const api = hbjs(source.instance);
 // const buffer = await fs.readFile("../../fonts/SourceHanSerifCN-Light.otf");
-const buffer = await fs.readFile("../../fonts/SmileySans-Oblique.ttf");
+const buffer = await fs.readFile('../../fonts/SmileySans-Oblique.ttf');
 const blob = api.createBlob(buffer);
 const face = api.createFace(blob);
 const arr = face.collectUnicodes().slice(0, 2001);
