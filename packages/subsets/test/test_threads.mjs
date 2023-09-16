a0718ef585d7095649c79ae93b5530e3a8aad0d8
江夏尧 <3446798488@qq.com>
2023-09-16 19:53:29 +0800

    feat: 调整部分算法

--- packages/subsets/test/test_threads.mjs
+++ packages/subsets/test/test_threads.mjs
@@ -6,8 +6,8 @@ import { fontSplit } from '../dist/index.js';
 fs.emptyDirSync('./temp');
 fontSplit({
     destFold: './temp',
-    // FontPath: '../demo/public/SmileySans-Oblique.ttf',
-    FontPath: './test/temp/江西拙楷.ttf',
+    FontPath: '../demo/public/SmileySans-Oblique.ttf',
+    // FontPath: './test/temp/江西拙楷.ttf',
     // FontPath: './test/temp/MaokenAssortedSans1.30.ttf',
     // FontPath: './test/temp/字魂扁桃体.ttf',
     // FontPath: './test/temp/京華老宋体v1.007.ttf',
@@ -16,6 +16,7 @@ fontSplit({
     targetType: 'woff2',
     // subsets: JSON.parse(await fs.readFile("./subsets/misans.json", "utf-8")),
     previewImage: {},
+    chunkSize: 70 * 1024,
     // autoChunk: false,
     // subsets: [[31105, 8413]],
     threads: {},
