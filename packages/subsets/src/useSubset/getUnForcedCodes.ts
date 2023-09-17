5da1663c2174cb695ccb10a50e1589ef7c83ef13
江夏尧 <3446798488@qq.com>
2023-09-17 08:53:26 +0800

    refractor: 拆分具体分包逻辑

---
+++ packages/subsets/src/useSubset/getUnForcedCodes.ts
@@ -0,0 +1,13 @@
+/** 获取未强制分包的 unicodes */
+export const getUnForcedCodes = (totalChars: Uint32Array, forcePartSet: Set<number>) => {
+    if (forcePartSet.size === 0) return [...totalChars];
+    /** 求出未分包的 unicodes */
+    const codes: number[] = [];
+    for (let index = 0; index < totalChars.length; index++) {
+        const element = totalChars[index];
+        if (!forcePartSet.has(element)) {
+            codes.push(element);
+        }
+    }
+    return codes;
+};
---
+++ packages/subsets/src/useSubset/reduceMinsPackage.ts
@@ -0,0 +1,21 @@
+import { Context } from '../pipeline/index';
+import { findOutliers } from './findOutliers';
+
+export const reduceMinsPackage = (fullSubsets: number[][], ctx: Context<unknown>) => {
+    // 清理整个分包算法结果中的离群最小值
+    const [mins, largePart, min] = findOutliers(fullSubsets, fullSubsets.map(i => i.length), 1);
+    const minsLength = mins.length;
+    if (!mins.length) return fullSubsets;
+
+    const combinedMinsPart = mins.sort((a, b) => a.length - b.length).reduce((col, cur) => {
+        const last = col[col.length - 1];
+        if (last.length + cur.length <= min * 1.1) {
+            last.push(...cur);
+        } else {
+            col.push([...cur]);
+        }
+        return col;
+    }, [[]] as number[][]);
+    ctx.info(`减少分包碎片 ${minsLength} => ${combinedMinsPart.length}  `);
+    return [...combinedMinsPart, ...largePart];
+};
