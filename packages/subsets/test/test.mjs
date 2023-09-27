21ec7f235c9ba5e33ae9d636953d9d7d1caeea60
江夏尧 <3446798488@qq.com>
2023-09-27 21:35:50 +0800

    feat: 升级 harfbuzz

--- packages/subsets/test/test.mjs
+++ packages/subsets/test/test.mjs
@@ -1,13 +1,13 @@
-import fs from "fs-extra";
-import { fontSplit } from "../dist/index.js";
+import fs from 'fs-extra';
+import { fontSplit } from '../dist/index.js';
 // const buffer = await fs.readFile(
 //     "./node_modules/@konghayao/harfbuzzjs/hb-subset.wasm"
 // );
-fs.emptyDirSync("./temp");
+fs.emptyDirSync('./temp');
 fontSplit({
-    destFold: "./temp",
-    FontPath: "../demo/public/SmileySans-Oblique.ttf",
-    targetType: "woff2",
+    destFold: './temp',
+    FontPath: '../demo/public/SmileySans-Oblique.ttf',
+    targetType: 'woff2',
     // subsets: JSON.parse(await fs.readFile("./subsets/misans.json", "utf-8")),
     previewImage: {},
     // logger: { settings: { minLevel: 2 } },
