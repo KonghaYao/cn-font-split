6f9962a488388e2025346f1e1a048c68b8156b36
江夏尧 <3446798488@qq.com>
2023-10-15 08:36:57 +0800

    feat: 添加版本号导出

--- packages/subsets/src/index.ts
+++ packages/subsets/src/index.ts
@@ -10,6 +10,8 @@ export * from './utils/env';
 export * from './templates/device';
 export type { ReporterFile } from './templates/reporter';
 
+export const VERSION_CN_FONT_SPLIT = __cn_font_split_version__;
+
 // 条件编译，只有在 Deno 环境下需要这个
 //ifdef browser
 export * from './adapter/deno/index';
