5dea7f0a64d6e8b0534060f0f84528bb1c44c6f7
江夏尧 <3446798488@qq.com>
2023-10-16 21:21:36 +0800

    feat: 使用 @nap-rs/ttf2woff2 作为node环境的 woff2 压缩器

--- packages/subsets/src/convert/convert.worker.ts
+++ packages/subsets/src/convert/convert.worker.ts
@@ -1,4 +1,3 @@
-import { convert } from './font-converter';
 import type { FontType } from '../utils/detectFormat';
 import { worker, Transfer } from 'workerpool';
 // 欺骗 环境，认为是 classic worker
@@ -7,7 +6,19 @@ import { worker, Transfer } from 'workerpool';
         console.warn('触发 importScripts 的伪装，可能导致 bug，', args);
     });
 
+let convert: (
+    buffer: Uint8Array,
+    toFormat: FontType,
+    fromFormat?: FontType
+) => Promise<Uint8Array>;
+//ifdef node
+import { convert as nodeConvert } from './font-converter.node';
+convert = nodeConvert;
+//endif
+
 //ifdef browser
+import { convert as commonConvert } from './font-converter';
+convert = commonConvert;
 import { DenoAdapter } from '../adapter/deno/index';
 import '../adapter/browser/URL.shim'; // 为了防止全局状态中 base 出现 blob 而导致的 URL 解析错误
 await DenoAdapter();
---
+++ packages/subsets/src/convert/font-converter.node.ts
@@ -0,0 +1,47 @@
+import {
+    FontType,
+    supportedFormats,
+    detectFormat,
+} from '../utils/detectFormat';
+import {
+    convertTTFToWOFF2Async,
+    convertWOFF2ToTTFAsync,
+} from '@napi-rs/ttf2woff2';
+
+/** 字体格式转化, node 特供版本，速度非常快 */
+export const convert = async function (
+    buffer: Uint8Array,
+    toFormat: FontType,
+    fromFormat?: FontType
+) {
+    const snft = ['truetype', 'ttf', 'otf'];
+    if (snft.includes(toFormat)) {
+        toFormat = 'sfnt';
+    }
+    if (snft.includes(fromFormat || '')) {
+        fromFormat = 'sfnt';
+    }
+    if (!supportedFormats.has(toFormat)) {
+        throw new Error(`Unsupported target format: ${toFormat}`);
+    }
+    if (fromFormat) {
+        if (!supportedFormats.has(fromFormat)) {
+            throw new Error(`Unsupported source format: ${fromFormat}`);
+        }
+    } else {
+        fromFormat = detectFormat(buffer);
+    }
+    if (fromFormat === toFormat) {
+        return buffer;
+    }
+    if (fromFormat === 'woff') {
+        // buffer = woffTool.toSfnt(buffer);
+        throw new Error('Unsupported source format: woff');
+    } else if (fromFormat === 'woff2') {
+        buffer = await convertWOFF2ToTTFAsync(buffer as Buffer);
+    }
+    if (toFormat === 'woff2') {
+        buffer = await convertTTFToWOFF2Async(buffer as Buffer);
+    }
+    return buffer;
+};
