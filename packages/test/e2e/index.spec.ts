7b8d0b03514b4003fd5ae646e8318fe464c71cd2
3446798488 <3446798488@qq.com>
2023-10-26 06:57:12 +0000

    feat: 添加视觉测试代码

---
+++ packages/test/e2e/index.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test('compare font style', async ({ page }) => {
+    await page.goto('https://zoplvy-ppxbvd-5173.preview.myide.io/');
+    await page.waitForLoadState('networkidle');
+    const fonts = await page.locator('.example-font');
+
+    const fontIndex = await fonts.count();
+    await fonts.nth(0).screenshot({ path: './temp/test-font.png' });
+    for (let i = 1; i < fontIndex; ++i) {
+        const item = await fonts
+            .nth(i)
+            .screenshot({ path: `./temp/test-font-${i}.png` });
+        await expect(item).toMatchSnapshot('./temp/test-font.png');
+    }
+});
