afe4a3750a14ee4a985a5f5ee13fe27d4e71954a
3446798488 <3446798488@qq.com>
2023-10-26 01:02:26 +0000

    feat: 添加测试对照字体显示

--- packages/test/src/App.tsx
+++ packages/test/src/App.tsx
@@ -1,6 +1,6 @@
-import { atom, resource } from '@cn-ui/reactive';
+import { resource } from '@cn-ui/reactive';
 import './App.css';
-
+import linkURL from './style/baseFont.css?url';
 export default () => {
     const testText =
         'cn-font-split 是中文网字计划 所使用的字体分包工具，通过高性能的各种技术将庞大的字体包拆分为适合网络分发的版本。经过四个版本的字体研究与代码迭代，这项技术在我们的网站中得到了充分的应用，实现了中文字体在 Web 领域的加载速度与效率的双飞跃。';
@@ -11,12 +11,16 @@ export default () => {
         {
             source: './deno/',
         },
-        {
-            source: './bun/',
-        },
     ];
     return (
         <section>
+            <FontRenderRow
+                index={-1}
+                source={''}
+                testText={testText}
+                link={linkURL}
+                familyName="Smiley Sans Oblique"
+            ></FontRenderRow>
             {models.map((i, index) => {
                 return (
                     <FontRenderRow
@@ -33,25 +37,36 @@ export default () => {
 export const FontRenderRow = (props: {
     source: string;
     index: number;
+    link?: string;
+    familyName?: string;
     testText: string;
 }) => {
-    const familyName = 'i' + crypto.randomUUID().replace(/\-/g, '');
-    const text = resource(() => {
-        return fetch(props.source + 'result.css')
-            .then((res) => res.text())
-            .then((res) => {
-                return res
-                    .replace(/Smiley\sSans\sOblique/g, familyName)
-                    .replace(
-                        /\.\//g,
-                        new URL(props.source, location.href).toString()
-                    );
-            })
-            .then((res) => {
-                const blob = new Blob([res]);
-                return URL.createObjectURL(blob);
-            });
-    });
+    const familyName =
+        props.familyName ?? 'i' + crypto.randomUUID().replace(/\-/g, '');
+    const text = resource(
+        () => {
+            return fetch(props.source + 'result.css')
+                .then((res) => res.text())
+                .then((res) => {
+                    return res
+                        .replace(/Smiley\sSans\sOblique/g, familyName)
+                        .replace(
+                            /\.\//g,
+                            new URL(props.source, location.href).toString()
+                        );
+                })
+                .then((res) => {
+                    const blob = new Blob([res]);
+                    return URL.createObjectURL(blob);
+                });
+        },
+        { immediately: false }
+    );
+    if (props.link) {
+        text(props.link);
+    } else {
+        text.refetch();
+    }
     return (
         <div style="text-wrap: nowrap;">
             <span style={{ 'font-family': familyName }}>{props.testText}</span>
