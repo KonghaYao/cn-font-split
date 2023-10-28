ce503acebadc52a7c22e87bef8427e0725851712
江夏尧 <3446798488@qq.com>
2023-10-28 09:37:09 +0800

    test: 添加 harfbuzz wasm 版本对照

--- packages/test/e2e/feature.spec.ts
+++ packages/test/e2e/feature.spec.ts
@@ -10,6 +10,7 @@ for (const iterator of features) {
         await page.goto('http://localhost:5173/');
         await page.waitForLoadState('networkidle');
         // 测试 harfbuzz 版本只需要将 -demo 替换为 -hb 即可
+        // 测试 harfbuzz wasm 版本只需要将 -demo 替换为 -hb-wasm 即可
         const item2 = await page
             .locator('.' + iterator.featureKey + '-demo')
             .screenshot();
---
+++ packages/test/script/gen_hb_wasm.mjs
@@ -0,0 +1,78 @@
+import fs from 'fs-extra';
+import _ from 'lodash-es'
+import { convert } from '@konghayao/cn-font-split';
+const features = fs.readJSONSync('./FeatureConfig.json');
+
+const wasmBlob = await fs.readFile('./node_modules/@konghayao/cn-font-split/dist/browser/hb-subset.wasm')
+const { instance: { exports } } = await WebAssembly.instantiate(wasmBlob);
+
+
+// fs.emptyDirSync('./temp');
+for (const i of features) {
+    const head = `./temp/${i.featureKey}/${i.featureKey}`
+    const buffer = fs.readFileSync(head + `.woff2`)
+    const fontBlob = await convert(new Uint8Array(buffer), 'ttf');
+    fs.writeFileSync(head + `.ttf`, fontBlob)
+
+    const heapu8 = new Uint8Array(exports.memory.buffer);
+    const fontBuffer = exports.malloc(fontBlob.byteLength);
+    heapu8.set(new Uint8Array(fontBlob), fontBuffer);
+
+    /* Creating a face */
+    const blob = exports.hb_blob_create(fontBuffer, fontBlob.byteLength, 2/*HB_MEMORY_MODE_WRITABLE*/, 0, 0);
+    const face = exports.hb_face_create(blob, 0);
+    exports.hb_blob_destroy(blob);
+
+    /* Add your glyph indices here and subset */
+    const input = exports.hb_subset_input_create_or_fail();
+
+    const layoutFeatures = exports.hb_subset_input_set(
+        input,
+        6
+    );
+    exports.hb_set_clear(layoutFeatures);
+    exports.hb_set_invert(layoutFeatures);
+    const layoutScripts = exports.hb_subset_input_set(
+        input,
+        7
+    );
+    exports.hb_set_clear(layoutScripts);
+    exports.hb_set_invert(layoutScripts);
+
+
+    const unicode_set = exports.hb_subset_input_unicode_set(input);
+    for (const text of i.splitText) {
+        exports.hb_set_add(unicode_set, text.codePointAt(0));
+    }
+
+    // exports.hb_subset_input_set_drop_hints(input, true);
+    const subset = exports.hb_subset_or_fail(face, input);
+
+    /* Clean up */
+    exports.hb_subset_input_destroy(input);
+
+    /* Get result blob */
+    const resultBlob = exports.hb_face_reference_blob(subset);
+
+    const offset = exports.hb_blob_get_data(resultBlob, 0);
+    const subsetByteLength = exports.hb_blob_get_length(resultBlob);
+    if (subsetByteLength === 0) {
+        exports.hb_blob_destroy(resultBlob);
+        exports.hb_face_destroy(subset);
+        exports.hb_face_destroy(face);
+        exports.free(fontBuffer);
+        throw new Error(
+            'Failed to create subset font, maybe the input file is corrupted?'
+        );
+    }
+
+    // Output font data(Uint8Array)
+    const subsetFontBlob = heapu8.subarray(offset, offset + exports.hb_blob_get_length(resultBlob));
+    await fs.outputFile(head + "-hb-wasm.ttf", subsetFontBlob);
+
+    /* Clean up */
+    exports.hb_blob_destroy(resultBlob);
+    exports.hb_face_destroy(subset);
+    exports.hb_face_destroy(face);
+    exports.free(fontBuffer);
+}
