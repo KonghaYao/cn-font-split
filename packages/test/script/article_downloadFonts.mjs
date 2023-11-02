572e655939f188f501a0f1cb8feeffaa74cfab15
3446798488 <3446798488@qq.com>
2023-11-02 06:37:40 +0000

    test: 添加中心化字体文件缓存

--- packages/test/script/article_downloadFonts.mjs
+++ packages/test/script/article_downloadFonts.mjs
@@ -1,16 +1,12 @@
 import fs from 'fs-extra'
 import _ from 'lodash-es'
-fs.emptyDirSync('./temp/font');
-
-const getFont = _.memoize((fontLink) => {
-    return fetch(fontLink).then((res) => res.arrayBuffer());
-});
+import { fontStore } from './FileStore.mjs';
 
 [{
     url: "https://github.com/adobe-fonts/source-han-serif/raw/release/Variable/OTF/SourceHanSerifSC-VF.otf",
     key: "SourceHanSerifSC-VF"
 }].map(i =>
-    getFont(i.url)
+    fontStore.get(i.url)
         .then(buffer => {
             fs.outputFileSync('./temp/font/' + i.key + i.fontLink.replace(/.*\.(.*?)/g, '.$1'), new Uint8Array(buffer))
         }))
--- packages/test/script/downloadFonts.mjs
+++ packages/test/script/downloadFonts.mjs
@@ -1,19 +1,19 @@
-import fs from 'fs-extra'
+import fs from 'fs-extra';
 const features = fs.readJSONSync('./FeatureConfig.json');
-import _ from 'lodash-es'
-fs.emptyDirSync('./temp/font');
-
-const getFont = _.memoize((fontLink) => {
-    return fetch(fontLink).then((res) => res.arrayBuffer());
-})
+import _ from 'lodash-es';
+import { FileStore, fontStore } from './FileStore.mjs';
 
 // 下载测试所需要的所有字体
 for (const i of features) {
-    const path = './temp/' + i.featureKey + "/" + i.featureKey + i.fontLink.replace(/.*\.(.*?)/g, '.$1')
-    if (fs.existsSync(path)) continue
-    console.log(i.featureKey)
-    await getFont(i.fontLink)
-        .then(buffer => {
-            fs.outputFileSync(path, new Uint8Array(buffer))
-        })
+    fontStore
+        .get(i.fontLink)
+        .then((buffer) => {
+            const path =
+                './temp/' +
+                i.featureKey +
+                '/' +
+                i.featureKey +
+                i.fontLink.replace(/.*\.(.*?)/g, '.$1');
+            fs.outputFileSync(path, new Uint8Array(buffer));
+        });
 }
