309165fca5b9155d6fa2137a0b880162cdee6460
江夏尧 <3446798488@qq.com>
2023-11-06 22:53:13 +0800

    test: 添加多个浏览器测试环境

--- packages/test/playwright.config.ts
+++ packages/test/playwright.config.ts
@@ -16,9 +16,13 @@ export default defineConfig({
             name: 'chromium',
             use: { ...devices['Desktop Chrome'] },
         },
-        // {
-        //     name: 'firefox',
-        //     use: { ...devices['Desktop Firefox'] },
-        // },
+        {
+            name: 'firefox',
+            use: { ...devices['Desktop Firefox'] },
+        },
+        {
+            name: 'webkit',
+            use: { ...devices['Desktop Safari'] },
+        },
     ],
 });
