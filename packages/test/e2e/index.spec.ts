0d82e4cb0494c0b9ba8d6f675a982c482e0be58b
3446798488 <3446798488@qq.com>
2024-01-04 06:50:23 +0000

    test(emoji): 测试用例完成，未开始测试

--- packages/test/e2e/index.spec.ts
+++ packages/test/e2e/index.spec.ts
@@ -4,7 +4,7 @@ import fs from 'fs-extra';
 import P from 'pngjs';
 const PNG = P.PNG;
 test('多构建方式：成品测试', async ({ page }) => {
-    await page.goto('http://localhost:5173/#/article');
+    await page.goto('http://localhost:5173/#/article?type=multi-platform');
     await page.waitForLoadState('networkidle');
     const fonts = await page.locator('.example-font');
 
@@ -20,7 +20,7 @@ test('多构建方式：成品测试', async ({ page }) => {
     }
 });
 test('可变字重测试', async ({ page }) => {
-    await page.goto('http://localhost:5173/#/article');
+    await page.goto('http://localhost:5173/#/article?type=vf');
     await page.waitForLoadState('networkidle');
     await compareElAndSave(
         page,
@@ -29,3 +29,29 @@ test('可变字重测试', async ({ page }) => {
         './temp/SourceHanSerifSC-VF/base-demo.diff.png',
     );
 });
+const tag = [
+    'Smileys and emotions',
+    'People',
+    'Animals and nature',
+    'Food and drink',
+    'Travel and places',
+    'Activities and events',
+    'Objects',
+    'Symbols',
+    'Flags',
+];
+for (const iterator of tag) {
+    test('表情包字体' + iterator, async ({ page }) => {
+        await page.goto(
+            'http://localhost:5173/#/article?type=noto-color-emoji',
+        );
+        await page.waitForLoadState('networkidle');
+
+        await compareElAndSave(
+            page,
+            `#${iterator}-base`,
+            `#${iterator}-demo`,
+            `./temp/NotoColorEmoji/${iterator}.diff.png`,
+        );
+    });
+}
