71e1a82d7f0a52279567eb38e0a3e0165954be9c
江夏尧 <3446798488@qq.com>
2024-01-07 19:13:45 +0800

    fix(analyze): 修复获取的 feature 数据非 unicode 问题

--- packages/font-analyze/src/Feature/index.ts
+++ packages/font-analyze/src/Feature/index.ts
@@ -5,13 +5,23 @@ import { FeatureList } from './FeatureList';
 export const getFeatureReport = (font: Font, unicodeSet: Set<number>) => {
     return FeatureList.reduce(
         (col, i) => {
-            const arr = (font.substitution as any).getFeature(i);
-            if (arr && arr.length) col[i] = arr;
+            const arr: { sub: number | number[]; by: number | number[] }[] = (
+                font.substitution as any
+            ).getFeature(i);
+            const idToUnicode = (id: number) => font.glyphs.get(id).unicodes;
+            if (arr && arr.length)
+                col[i] = arr.map((i) => {
+                    return {
+                        sub: (typeof i.sub === 'number' ? [i.sub] : i.sub).map(
+                            (i) => idToUnicode(i),
+                        ),
+                        by: (typeof i.by === 'number' ? [i.by] : i.by).map(
+                            (i) => idToUnicode(i),
+                        ),
+                    };
+                });
             return col;
         },
-        {} as Record<
-            string,
-            { sub: number | number[]; by: number | number[] }[]
-        >,
+        {} as Record<string, { sub: number[][]; by: number[][] }[]>,
     );
 };
