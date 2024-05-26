17956df5dd6aa1ca9f8a19f0c43c0de673721f6b
江夏尧 <3446798488@qq.com>
2024-05-26 10:41:33 +0800

    fix: 修复 node 重复加载两个 woff2 压缩器问题

---
+++ packages/subsets/src/convert/commonConvert.ts
@@ -0,0 +1,19 @@
+let convert: (
+    buffer: Uint8Array,
+    toFormat: FontType,
+    fromFormat?: FontType,
+) => Promise<Uint8Array>;
+//ifdef node
+import { convert as nodeConvert } from './font-converter.node';
+convert = nodeConvert;
+//endif
+
+//ifdef browser
+import { convert as commonConvert } from './font-converter';
+convert = commonConvert;
+import { DenoAdapter } from '../adapter/deno/index';
+import '../adapter/browser/URL.shim'; // 为了防止全局状态中 base 出现 blob 而导致的 URL 解析错误
+import { FontType } from '../utils/detectFormat';
+await DenoAdapter();
+//endif
+export { convert };
