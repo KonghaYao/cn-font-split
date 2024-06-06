f2dc3c3412703a84706020348175210097781696
江夏尧 <3446798488@qq.com>
2024-06-06 19:52:57 +0800

    63 window环境下nextjs使用vite plugin font会解析路径失败 (#64)
    * fix: 修复 windows 路径问题

--- packages/vite/src/webpack.mts
+++ packages/vite/src/webpack.mts
@@ -1,17 +1,18 @@
-import { SubsetBundlePlugin, SubsetUtils } from "./subset/SubsetBundlePlugin.js";
-export default async function () {
+import {
+    SubsetBundlePlugin,
+    SubsetUtils,
+} from './subset/SubsetBundlePlugin.js';
+export default async function (this: any) {
     const config = this.getOptions();
-    const cacheDir = "node_modules/.cache/.font";
+    const cacheDir = 'node_modules/.cache/.font';
     config.cacheDir = config.cacheDir || cacheDir;
     const id = this.resourcePath + this.resourceQuery;
 
-
-
-    const { isSubset, searchParams } = SubsetUtils.isSubsetLink(id)
-    const key = searchParams.get('key') ?? 'default'
-    let plugin = new SubsetBundlePlugin(config)
-    plugin.key = key
+    const { isSubset, searchParams } = SubsetUtils.isSubsetLink(id);
+    const key = searchParams.get('key') ?? 'default';
+    let plugin = new SubsetBundlePlugin(config);
+    plugin.key = key;
     await plugin.createSubsets();
-    await plugin.createBundle(id, isSubset ? "subsets" : "full");
+    await plugin.createBundle(id, isSubset ? 'subsets' : 'full');
     return plugin.createSourceCode(id);
 }
--- packages/vite/test/build.test.mjs
+++ packages/vite/test/build.test.mjs
@@ -2,33 +2,55 @@ import fs from 'fs-extra';
 import { exec } from 'child_process';
 import assert from 'assert';
 // 执行 pnpm vite build 并检查
-fs.emptyDirSync('./node_modules/.vite')
-
+fs.emptyDirSync('./node_modules/.vite');
+fs.emptyDirSync('./node_modules/.cache');
 
 await new Promise((res, rej) => {
-  exec('pnpm vite build', { cwd: process.cwd() }, (err, stdout, stderr) => {
+    exec('pnpm vite build', { cwd: process.cwd() }, (err, stdout, stderr) => {
         if (err) {
             rej(err);
             return;
         }
         res(`stdout: ${stdout}`);
     });
-})
+});
 
-assert.ok(fs.existsSync('./node_modules/.vite/.subsets/2f32afea01e31e457a7be1dabf93e228/SmileySans-Oblique_ttf/reporter.json'))
-assert.ok(fs.existsSync('./node_modules/.vite/.subsets/1851a5900af7816b43bd1b9aefd9da9f/SmileySans-Oblique_ttf/reporter.json'))
-assert.ok(fs.existsSync('./node_modules/.vite/SmileySans-Oblique_ttf/reporter.json'))
+assert.ok(
+    fs.existsSync(
+        './node_modules/.vite/.subsets/2f32afea01e31e457a7be1dabf93e228/SmileySans-Oblique_ttf/reporter.json',
+    ),
+);
+assert.ok(
+    fs.existsSync(
+        './node_modules/.vite/.subsets/1851a5900af7816b43bd1b9aefd9da9f/SmileySans-Oblique_ttf/reporter.json',
+    ),
+);
+assert.ok(
+    fs.existsSync('./node_modules/.vite/SmileySans-Oblique_ttf/reporter.json'),
+);
 
 await new Promise((res, rej) => {
-  exec('pnpm rspack build', { cwd: process.cwd() }, (err, stdout, stderr) => {
+    exec('pnpm rspack build', { cwd: process.cwd() }, (err, stdout, stderr) => {
         if (err) {
             rej(err);
             return;
         }
         res(`stdout: ${stdout}`);
     });
-})
+});
 
-assert.ok(fs.existsSync('./node_modules/.cache/.font/.subsets/2f32afea01e31e457a7be1dabf93e228/SmileySans-Oblique_ttf/reporter.json'))
-assert.ok(fs.existsSync('./node_modules/.cache/.font/.subsets/1851a5900af7816b43bd1b9aefd9da9f/SmileySans-Oblique_ttf/reporter.json'))
-assert.ok(fs.existsSync('./node_modules/.cache/.font/SmileySans-Oblique_ttf/reporter.json'))
+assert.ok(
+    fs.existsSync(
+        './node_modules/.cache/.font/.subsets/2f32afea01e31e457a7be1dabf93e228/SmileySans-Oblique_ttf/reporter.json',
+    ),
+);
+assert.ok(
+    fs.existsSync(
+        './node_modules/.cache/.font/.subsets/1851a5900af7816b43bd1b9aefd9da9f/SmileySans-Oblique_ttf/reporter.json',
+    ),
+);
+assert.ok(
+    fs.existsSync(
+        './node_modules/.cache/.font/SmileySans-Oblique_ttf/reporter.json',
+    ),
+);
