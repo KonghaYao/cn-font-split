2b2bcb26923a63c54b822094f8886faf3797fb24
江夏尧 <3446798488@qq.com>
2024-06-18 20:56:39 +0800

    feat: 添加字体度量库

---
+++ packages/metrics/fontaine.mjs
@@ -0,0 +1,28 @@
+import { readMetrics } from 'fontaine';
+import { glob } from 'glob';
+import { pathToFileURL } from 'url';
+
+const fonts = glob.globSync('./fonts/**/*.{otf,ttf}', { absolute: true });
+
+let code = '';
+
+const codeTemplate = (key, metrics) => `
+export const ${key.replace('-', '')} = ${JSON.stringify(metrics)};`;
+
+for (const font of fonts) {
+    const metrics = await readMetrics(pathToFileURL(font));
+
+    code += codeTemplate(font.split('/').pop().split('.')[0], metrics);
+}
+
+// const metrics = await readMetrics(
+//     pathToFileURL('../../demo/public/SmileySans-Oblique.ttf'),
+// );
+// const fontFace = generateFontFace(metrics, {
+//     name: '_fallback',
+//     font: 'NotoSerifSC-Regular',
+//     metrics: fallbackMetrics,
+// });
+
+import fs from 'fs';
+fs.writeFileSync('./metrics.ts', code);
