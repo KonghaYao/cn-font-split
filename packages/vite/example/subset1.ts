703ffeea63a9561763dfd043a9afccec6a77c108
3446798488 <3446798488@qq.com>
2024-06-19 01:57:42 +0000

    test: 更改 vite 插件的测试环境

--- packages/vite/example/subset1.ts
+++
@@ -1,5 +0,0 @@
-// import '../src/font.d'
-import { css } from '../../demo/public/SmileySans-Oblique.ttf?subsets&key=page-1';
-
-document.body.innerHTML = JSON.stringify(css, null, 4);
-export const a = 1;
