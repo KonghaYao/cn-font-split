395c8493a4d4c7f174eae168519c4ea7b6da068c
3446798488 <3446798488@qq.com>
2024-07-05 07:03:31 +0000

    refactor: 重构 utils 代码

--- packages/subsets/src/useSubset/recordToLog.ts
+++ packages/subsets/src/useSubset/recordToLog.ts
@@ -1,7 +1,11 @@
-import { timeRecordFormat } from '../utils/timeCount';
 import byteSize from 'byte-size';
 import { IContext } from '../ createContext';
 
+const timeRecordFormat = (start: number, end: number) => {
+    return (end - start).toFixed(0) + 'ms';
+};
+
+/** 记录到日志里面，用于 cli 中展示 */
 export async function recordToLog(
     ctx: IContext,
     transferred: Uint8Array,
--- packages/subsets/src/utils/timeCount.ts
+++
@@ -1,3 +0,0 @@
-export const timeRecordFormat = (start: number, end: number) => {
-    return (end - start).toFixed(0) + 'ms';
-};
