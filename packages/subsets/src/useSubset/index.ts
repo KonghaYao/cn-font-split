d08c0e63489012932bc9d2dd6372a805957d2fff
江夏尧 <3446798488@qq.com>
2024-07-06 09:48:50 +0800

    refactor: 重构部分逻辑

--- packages/subsets/src/useSubset/index.ts
+++ packages/subsets/src/useSubset/index.ts
@@ -7,10 +7,6 @@ import { getExtensionsByFontType } from '../convert/detectFormat';
 import { subsetFont } from './subsetFont';
 import { createRecord } from '../logger/createRecord';
 import { recordToLog } from '../logger/recordToLog';
-import {
-    FeatureMap,
-    processSingleUnicodeWithFeature,
-} from '../feature/featureMap';
 
 /** 直接根据chunk 批量分包字体 */
 export const useSubset = async (
@@ -127,40 +123,3 @@ async function runSubSet({
         outputMessage.path,
     );
 }
-
-/** 获取自动分包方案 */
-export const getAutoSubset = (
-    subsetUnicode: number[],
-    contoursBorder: number,
-    contoursMap: Map<number, number>,
-    featureMap: FeatureMap,
-    maxCharSize: number,
-) => {
-    let count = 0;
-    let cache: number[] = [];
-    const totalChunk: number[][] = [];
-
-    const defaultVal = contoursMap.get(0) as number;
-    for (const unicode of subsetUnicode) {
-        // featureMap 已经进行了去重
-        const unicodeSet = processSingleUnicodeWithFeature(unicode, featureMap);
-
-        const sum = unicodeSet.reduce(
-            (col, cur) => col + (contoursMap.get(cur) ?? defaultVal),
-            0,
-        );
-        // contoursMap 0 是平均值
-        count += sum;
-        cache.push(...unicodeSet);
-
-        if (count >= contoursBorder || cache.length >= maxCharSize) {
-            totalChunk.push(cache);
-            cache = [];
-            count = 0;
-        }
-    }
-    if (cache.length) totalChunk.push(cache);
-
-    // console.log(totalChunk.flat().length);
-    return totalChunk;
-};
