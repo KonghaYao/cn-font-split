65af2b92353231eb253fcf81088d9ac26e5348f4
江夏尧 <3446798488@qq.com>
2024-07-13 17:24:22 +0800

    fix(font-sharp): 更新harfbuzzjs依赖并修复WASM加载逻辑

--- packages/font-sharp/src/font2svg.ts
+++ packages/font-sharp/src/font2svg.ts
@@ -1,27 +1,13 @@
 import { hbjs } from '../../subsets/src/hb.js';
-import fs from 'fs';
 import { Font2SVGOptions, makeImage } from './makeImage.js';
-import { createRequire } from 'module';
-import { fileURLToPath } from 'url';
-import path from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const buf = fs.readFileSync(
-    createRequire(path.resolve(__dirname)).resolve(
-        '@konghayao/harfbuzzjs/hb.wasm',
-    ),
-);
-const wasm = WebAssembly.instantiate(buf);
+import wrapper from '@konghayao/harfbuzzjs/hb.js';
 
 export const font2svg = async (
     ttfFile: Uint8Array,
     text: string,
     options?: Font2SVGOptions,
 ) => {
-    if (!wasm) throw new Error('启动 harfbuzz 失败');
-    const hb = hbjs((await wasm).instance);
+    const hb = hbjs(await wrapper());
     const blob = hb.createBlob(ttfFile);
     const face = hb.createFace(blob, 0);
     blob.destroy();
