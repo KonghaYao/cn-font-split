35ccd649487fdfa0faad149cad7a2a2fbe588462
江夏尧 <3446798488@qq.com>
2024-11-28 06:49:50 +0000

    feat: 添加 svg 直接创建

--- packages/subsets-rs/src/pre_subset/gen_svg.rs
+++ packages/subsets-rs/src/pre_subset/gen_svg.rs
@@ -1,6 +1,13 @@
 use harfbuzz_rs_now::{Face, Font, Owned};
 
 pub fn gen_svg(face: &Owned<Face<'_>>, text: &str) -> String {
-    let mut font = Font::new(Face::new(face.face_data(),0));
+    let mut font = Font::new(Face::new(face.face_data(), 0));
+    font.render_svg_text(text, &[])
+}
+
+/// 直接从字体文件创建 svg 字符串
+pub fn gen_svg_from_font_file(file: &[u8], text: &str) -> String {
+    let face = Face::from_bytes(file, 0);
+    let mut font = Font::new(face);
     font.render_svg_text(text, &[])
 }
