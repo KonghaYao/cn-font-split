36fe014b831803ba50d6fb1df9e79f5cf3cc20e5
江夏尧 <3446798488@qq.com>
2024-12-04 06:34:15 +0000

    feat: 添加 wasm-opt 安装方式

--- packages/subsets-rs/.devcontainer/wasi-install.sh
+++ packages/subsets-rs/.devcontainer/wasi-install.sh
@@ -12,4 +12,4 @@ tar xvf wasi-sdk-${WASI_VERSION_FULL}-${WASI_ARCH}-${WASI_OS}.tar.gz -C /opt/was
 rm -rf wasi-sdk-${WASI_VERSION_FULL}-${WASI_ARCH}-${WASI_OS}.tar.gz
 export WASI_SYSROOT="/opt/wasi-sdk/wasi-sdk-${WASI_VERSION_FULL}-${WASI_ARCH}-${WASI_OS}/share/wasi-sysroot"
 echo -e "\nexport WASI_SYSROOT=\"${WASI_SYSROOT}\"\n" >> ~/.bashrc
-echo -e "\nexport WASI_SDK_PATH=\"/opt/wasi-sdk/wasi-sdk-${WASI_VERSION_FULL}-${WASI_ARCH}-${WASI_OS}/\"\n" >> ~/.zshrc
+echo -e "\nexport WASI_SDK_PATH=\"/opt/wasi-sdk/wasi-sdk-${WASI_VERSION_FULL}-${WASI_ARCH}-${WASI_OS}/\"\n" >> ~/.bashrc
