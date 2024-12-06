9fd0967a88c1fcc661ce22a4f6a778318a81a9c4
huangxn27 <huangxn27@annto.com.cn>
2024-12-06 09:01:38 +0000

    feat: 更新服务

--- packages/subsets-rs/packages/server/src/main.rs
+++ packages/subsets-rs/packages/server/src/main.rs
@@ -1,4 +1,9 @@
-use std::convert::Infallible;
+use std::{
+    convert::Infallible,
+    env,
+    io::Write,
+    sync::{Arc, Mutex},
+};
 
 use axum::{
     body::{Body, Bytes},
@@ -11,8 +16,10 @@ use axum::{
 mod storage;
 use cn_font_proto::api_interface::{EventMessage, EventName, InputTemplate};
 use cn_font_split::font_split;
+use storage::OssApi;
 use tokio::sync::mpsc::{self};
 use tokio_stream::StreamExt as _;
+use zip::write::FileOptions;
 
 #[tokio::main]
 async fn main() {
@@ -35,14 +42,48 @@ async fn upload(file: Bytes) -> Response<Body> {
     headers.insert("Content-Type", "text/event-stream".parse().unwrap());
     headers.insert("Cache-Control", "no-cache".parse().unwrap());
     headers.insert("Connection", "keep-alive".parse().unwrap());
+    
+    let oss = OssApi::new(&env::var("S3_ENDPOINT").unwrap());
+    oss.init_font_system().await;
 
     let (tx, mut rx) = mpsc::unbounded_channel::<Event>();
     // 启动异步任务，该任务会调用回调函数并将数据发送到通道中
     tokio::spawn(async move {
+        let mut buffer = Vec::new();
+        let hash = format!("{:?}", md5::compute(buffer.as_slice()));
+        let zip_writer = zip::ZipWriter::new(std::io::Cursor::new(&mut buffer));
+        let safe_zip = Arc::new(Mutex::new(zip_writer));
+        let result_font = oss.get_bucket("result-font");
         font_split(template, |data: EventMessage| {
-            let _ = tx.send(event_message_to_event(data));
+            let a = Arc::clone(&safe_zip);
+            let mut safe_zip = a.lock().unwrap();
+            match EventName::try_from(data.event).unwrap() {
+                EventName::Unspecified => {}
+                EventName::OutputData => {
+                    safe_zip
+                        .start_file::<String, ()>(
+                            data.message.clone(),
+                            FileOptions::default(),
+                        )
+                        .unwrap();
+                    let binary = data.data.unwrap();
+                    safe_zip.write_all(&binary).unwrap();
+                }
+                EventName::End => {}
+            };
+
+            let event = EventName::try_from(data.event).unwrap().as_str_name();
+            let _ =
+                tx.send(Event::default().id(data.message.clone()).event(event));
         });
+
+        let safe_zip =
+            Arc::try_unwrap(safe_zip).ok().unwrap().into_inner().unwrap();
+        safe_zip.finish().unwrap();
+        let _ = result_font.put_object(hash.clone() + ".zip", &buffer).await;
+        let _ = tx.send(Event::default().id("result").event(hash));
     });
+
     let sse_stream = async_stream::stream! {
         // 创建一个无缓冲的通道
         while let Some(item) = rx.recv().await {
@@ -54,10 +95,4 @@ async fn upload(file: Bytes) -> Response<Body> {
     let sse_response =
         Sse::new(try_str).keep_alive(axum::response::sse::KeepAlive::new());
     sse_response.into_response()
-}
-
-fn event_message_to_event(message: EventMessage) -> Event {
-    // let binding = message.data.unwrap();
-    let event = EventName::try_from(message.event).unwrap().as_str_name();
-    Event::default().id(message.message).event(event)
 }
--- packages/subsets-rs/packages/server/src/storage.rs
+++ packages/subsets-rs/packages/server/src/storage.rs
@@ -1,8 +1,7 @@
-use std::collections::HashMap;
-use std::env;
 use s3::creds::Credentials;
 use s3::{bucket, Bucket, BucketConfiguration, Region};
-
+use std::collections::HashMap;
+use std::env;
 
 pub struct OssApi {
     pub base_url: String,
@@ -10,31 +9,36 @@ pub struct OssApi {
     pub credentials: Credentials,
 }
 impl OssApi {
-    fn new(base_url: &str) -> OssApi {
+    pub fn new(base_url: &str) -> OssApi {
         let s3_ak = env::var("S3_AK").expect("S3_AK must be set");
         let s3_sk = env::var("S3_SK").expect("S3_SK must be set");
         let s3_region = env::var("S3_REGION").unwrap_or("us-west-2".to_owned());
-        let s3_endpoint = env::var("S3_ENDPOINT").expect("S3_ENDPOINT must be set");
-        let region = Region::Custom {
-            region: s3_region,
-            endpoint: s3_endpoint,
-        };
+        let s3_endpoint =
+            env::var("S3_ENDPOINT").expect("S3_ENDPOINT must be set");
+        let region =
+            Region::Custom { region: s3_region, endpoint: s3_endpoint };
         let credentials = Credentials::new(
             Some(&s3_ak),
             Some(&s3_sk),
             None,
             None,
             Some(&"static".to_string()),
-        ).unwrap();
-        OssApi {
-            base_url: base_url.to_string(),
-            credentials,
-            region,
-        }
+        )
+        .unwrap();
+        OssApi { base_url: base_url.to_string(), credentials, region }
     }
-    async fn create_bucket(&self, bucket_name: &str, config: BucketConfiguration) -> Box<Bucket> {
-        let bucket =
-            Bucket::new(bucket_name, self.region.clone(), self.credentials.clone()).unwrap().with_path_style();
+    pub async fn create_bucket(
+        &self,
+        bucket_name: &str,
+        config: BucketConfiguration,
+    ) -> Box<Bucket> {
+        let bucket = Bucket::new(
+            bucket_name,
+            self.region.clone(),
+            self.credentials.clone(),
+        )
+        .unwrap()
+        .with_path_style();
 
         if !bucket.exists().await.unwrap() {
             let res = Bucket::create_with_path_style(
@@ -43,7 +47,8 @@ impl OssApi {
                 self.credentials.clone(),
                 config.clone(),
             )
-                .await.unwrap();
+            .await
+            .unwrap();
             println!("create bucket => {}", bucket_name);
             res.bucket
         } else {
@@ -52,13 +57,15 @@ impl OssApi {
         }
     }
     pub fn get_bucket(&self, bucket_name: &str) -> Box<Bucket> {
-        Bucket::new(bucket_name, self.region.clone(), self.credentials.clone()).unwrap().with_path_style()
+        Bucket::new(bucket_name, self.region.clone(), self.credentials.clone())
+            .unwrap()
+            .with_path_style()
     }
 }
 
 impl OssApi {
     /// 初始化系统，将会自动查找并创建桶
-    async fn init_font_system(&self) -> bool {
+    pub async fn init_font_system(&self) -> bool {
         self.create_bucket("origin-font", BucketConfiguration::default()).await;
         self.create_bucket("result-font", BucketConfiguration::default()).await;
 
@@ -66,16 +73,16 @@ impl OssApi {
     }
 }
 
-fn init_env() -> OssApi {
+fn init_test_env() {
     env::set_var("S3_ENDPOINT", "https://play.min.io:9000");
     env::set_var("S3_AK", "Q3AM3UQ867SPQQA43P2F");
     env::set_var("S3_SK", "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG");
     env::set_var("S3_REGION", "us-east-1");
-    OssApi::new("https://play.min.io:9000/")
 }
 #[tokio::test]
 async fn upload_file() {
-    let oss = init_env();
+    init_test_env();
+    let oss = OssApi::new("https://play.min.io:9000/");
     oss.init_font_system().await;
     let bucket = oss.get_bucket("origin-font");
     let s3_path = "test.file";
