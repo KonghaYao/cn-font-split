722b4ccfc51cbcd5904198598811c856e2c8f8e8
江夏尧 <3446798488@qq.com>
2024-12-08 16:11:18 +0800

    feat: 完善 wasm 版本

--- packages/subsets-rs/packages/wasm-edge/src/index.test.ts
+++ packages/subsets-rs/packages/wasm-edge/src/index.test.ts
@@ -1,22 +1,21 @@
-import { fontSplit } from './index';
+import { fontSplit, StaticWasm } from './index';
 const input = await fetch(
     'https://jsdelivr.deno.dev/gh/KonghaYao/cn-font-split/packages/demo/public/SmileySans-Oblique.ttf',
 ).then((res) => res.arrayBuffer());
-fontSplit(
+
+const wasm = new StaticWasm('./target/wasm32-wasip1/release/wasm_edge.wasm');
+
+const data = await fontSplit(
     {
         input: new Uint8Array(input),
     },
-    (imports) =>
-        WebAssembly.instantiateStreaming(
-            fetch('./target/wasm32-wasip1/release/wasm_edge.wasm'),
-            // './target/wasm32-wasip1/release/wasm_edge.Oz.wasm',
-            imports as any,
-        ),
+    wasm.WasiHandle,
     {
         logger(str, type) {
             console.log(str);
         },
     },
-).then((res) => {
-    console.log(res);
-});
+);
+
+console.log(data);
+// { name: string, data: Uint8Array }[]
--- packages/subsets-rs/packages/wasm-edge/src/index.ts
+++ packages/subsets-rs/packages/wasm-edge/src/index.ts
@@ -1,7 +1,6 @@
-import { IDirent, WASI } from '@tybys/wasm-util';
-import 'buffer';
-import { IFs, Volume, createFsFromVolume } from 'memfs-browser';
 import { api_interface } from '../../ffi/gen/index';
+import { IFs, Volume, createFsFromVolume } from 'memfs-browser';
+import { WASI } from '@tybys/wasm-util';
 
 // @ts-ignore
 globalThis.process = {
@@ -14,6 +13,51 @@ export type FontSplitProps = ConstructorParameters<
     typeof api_interface.InputTemplate
 >[0];
 export { api_interface as proto };
+
+export class APIInterface {
+    constructor(
+        public key: string = Math.random().toString().replace('.', ''),
+    ) {}
+    fs: IFs;
+    async init(fs = createFsFromVolume(new Volume())) {
+        await fs.promises.mkdir('/tmp/fonts', { recursive: true });
+        await fs.promises.mkdir('/tmp/' + this.key);
+        this.fs = fs;
+    }
+    async setConfig(config: FontSplitProps) {
+        const i = new api_interface.InputTemplate(config);
+
+        await this.fs.promises.writeFile(
+            '/tmp/fonts/' + this.key,
+            i.serialize(),
+        );
+    }
+    async callback() {
+        const files = (await this.fs.promises.readdir(
+            '/tmp/' + this.key,
+        )) as string[];
+        return Promise.all(
+            files
+                .filter((i) => typeof i === 'string')
+                .map(async (file) => {
+                    if (file) {
+                        const path = '/tmp/' + this.key + '/' + file;
+                        const data = (await this.fs.promises.readFile(
+                            path,
+                        )) as Uint8Array;
+                        await this.fs.promises.unlink(path);
+                        return {
+                            name: file,
+                            data,
+                        };
+                    }
+                }),
+        ).finally(async () => {
+            await this.fs.promises.unlink('/tmp/fonts/' + this.key);
+        });
+    }
+}
+
 export async function fontSplit(
     input: FontSplitProps,
     loadWasm: (
@@ -25,16 +69,32 @@ export async function fontSplit(
         fs?: IFs;
     },
 ) {
-    const fs = options?.fs ?? createFsFromVolume(new Volume());
-    await fs.promises.mkdir('/tmp');
-    await fs.promises.mkdir('/tmp/fonts');
-    const key = options?.key ?? 'input';
-    const i = new api_interface.InputTemplate(input);
-    await fs.promises.mkdir('/tmp/' + key);
-    await fs.promises.writeFile('/tmp/fonts/' + key, i.serialize());
+    const api = new APIInterface(options?.key);
+    await api.init();
+    await api.setConfig(input);
 
+    const { imports, wasi } = createWasi(api, options);
+
+    const wasm = await loadWasm(imports);
+    const { instance } = wasm;
+    console.time('wasm');
+    await wasi.start(instance);
+    console.timeEnd('wasm');
+
+    return api.callback();
+}
+export function createWasi(
+    api: APIInterface,
+    options:
+        | {
+              key?: string;
+              logger: (str: string, type: 'log' | 'error') => void;
+              fs?: IFs;
+          }
+        | undefined,
+) {
     const wasi = new WASI({
-        args: [key],
+        args: [api.key],
         env: {
             WASI_SDK_PATH: '/opt/wasi-sdk',
             RUST_LOG: 'debug',
@@ -43,7 +103,7 @@ export async function fontSplit(
             '/': '/',
         },
         // @ts-ignore
-        fs,
+        fs: api.fs,
         print(text) {
             options?.logger(text, 'log');
         },
@@ -72,25 +132,20 @@ export async function fontSplit(
             },
         },
     };
+    return { imports, wasi };
+}
 
-    const { instance } = await loadWasm(imports);
-    console.time('wasm');
-    await wasi.start(instance);
-    console.timeEnd('wasm');
-    const files = (await fs.promises.readdir('/tmp/' + key)) as string[];
-    return Promise.all(
-        files
-            .filter((i) => typeof i === 'string')
-            .map(async (file) => {
-                if (file)
-                    return {
-                        name: file,
-                        data: (await fs.promises.readFile(
-                            '/tmp/' + key + '/' + file,
-                        )) as Uint8Array,
-                    };
-            }),
-    ).finally(() => {
-        // TODO 清理内存
-    });
+export class StaticWasm {
+    wasmBuffer: Promise<ArrayBuffer>;
+    constructor(public url: string) {
+        this.wasmBuffer = fetch(url).then((res) => res.arrayBuffer());
+    }
+    WasiHandle = async (imports: any) => {
+        return WebAssembly.instantiate(
+            new Uint8Array((await this.wasmBuffer).slice()),
+
+            // './target/wasm32-wasip1/release/wasm_edge.Oz.wasm',
+            imports as any,
+        );
+    };
 }
