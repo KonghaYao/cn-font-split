f3236a1c2ef6d50db2f6f046e363033ef246d344
江夏尧 <3446798488@qq.com>
2024-12-17 21:27:36 +0800

    test: 升级测试依赖

--- packages/test/e2e/index.spec.ts
+++ packages/test/e2e/index.spec.ts
@@ -1,24 +1,6 @@
 import { test, expect } from '@playwright/test';
 import { compareElAndSave, comparePictureBuffer } from './comparePictureBuffer';
-import fs from 'fs-extra';
-import P from 'pngjs';
-const PNG = P.PNG;
-test('多构建方式：成品测试', async ({ page }) => {
-    await page.goto('http://localhost:5173/#/article?type=multi-platform');
-    await page.waitForLoadState('networkidle');
-    const fonts = await page.locator('.example-font');
 
-    const fontIndex = await fonts.count();
-    const base = await fonts.nth(0).screenshot();
-    for (let i = 1; i < fontIndex; ++i) {
-        const item = await fonts.nth(i).screenshot();
-        const { pixelDiffCount, diff } = comparePictureBuffer(base, item, {
-            threshold: 0.3,
-        });
-        fs.outputFile('./temp/index-' + i + '.png', PNG.sync.write(diff));
-        expect(pixelDiffCount).toBeLessThanOrEqual(40);
-    }
-});
 test('可变字重测试', async ({ page }) => {
     await page.goto('http://localhost:5173/#/article?type=vf');
     await page.waitForLoadState('networkidle');
