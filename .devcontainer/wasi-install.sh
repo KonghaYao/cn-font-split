b754cd7242efabd07e83d2d6e1b707fea497b039
江夏尧 <3446798488@qq.com>
2025-11-10 21:01:40 +0800

    fix: 改为 27 版本

--- .devcontainer/wasi-install.sh
+++ .devcontainer/wasi-install.sh
@@ -2,7 +2,7 @@
 apt install wget
 WASI_OS=linux
 WASI_ARCH=x86_64
-WASI_VERSION=28
+WASI_VERSION=27
 WASI_VERSION_FULL=${WASI_VERSION}.0
 wget https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-${WASI_VERSION}/wasi-sdk-${WASI_VERSION_FULL}-${WASI_ARCH}-${WASI_OS}.tar.gz
 if [ ! -d "/opt/wasi-sdk" ]; then
