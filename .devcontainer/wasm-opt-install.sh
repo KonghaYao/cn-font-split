8ff9fef35f3604fc78177ddc0f3ae97ade4a0f03
江夏尧 <3446798488@qq.com>
2025-11-11 21:50:04 +0800

    feat: 添加对 wasm32-wasip2 支持

--- .devcontainer/wasm-opt-install.sh
+++ .devcontainer/wasm-opt-install.sh
@@ -1,7 +1,7 @@
-wget https://ghproxy.cn/github.com/WebAssembly/binaryen/releases/download/version_119/binaryen-version_119-x86_64-linux.tar.gz
+wget https://github.com/WebAssembly/binaryen/releases/download/version_124/binaryen-version_124-x86_64-linux.tar.gz
 if [ ! -d "/opt/binaryen/" ]; then
   mkdir /opt/binaryen/
 fi
-tar xvf binaryen-version_119-x86_64-linux.tar.gz -C /opt/binaryen/
-rm binaryen-version_119-x86_64-linux.tar.gz
-echo "\nexport PATH=\$PATH:/opt/binaryen/binaryen-version_119/bin\n" >> ~/.bashrc
+tar xvf binaryen-version_124-x86_64-linux.tar.gz -C /opt/binaryen/
+rm binaryen-version_124-x86_64-linux.tar.gz
+echo "\nexport PATH=\$PATH:/opt/binaryen/binaryen-version_124/bin\n" >> ~/.bashrc
--- src/loader.rs
+++ src/loader.rs
@@ -9,7 +9,7 @@ fn is_woff2(data: &[u8]) -> bool {
 }
 
 // 从字节数组中加载 WOFF2 字体，返回字节数组。若不是 WOFF2 格式，则返回原始字节数组。
-pub fn smart_load_woff2(data: &[u8]) -> Cow<[u8]> {
+pub fn smart_load_woff2(data: &[u8]) -> Cow<'_, [u8]> {
     if is_woff2(data) {
         Cow::Owned(decompress(data).expect("illegal woff2 file"))
     } else {
